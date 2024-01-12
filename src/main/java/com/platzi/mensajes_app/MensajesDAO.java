310d6fe65c442dae5f3a4a83aa4bd8bcb8fcece7
Tuta <solmaruboldi@gmail.com>
2024-01-11 21:17:58 -0300

    Agregué el método crear mensaje en DAO y Service

--- src/main/java/com/platzi/mensajes_app/MensajesDAO.java
+++ src/main/java/com/platzi/mensajes_app/MensajesDAO.java
@@ -1,9 +1,29 @@
 package com.platzi.mensajes_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
 
     public static void crearMensajeDB(Mensajes mensaje) {
+        Conexion db_connect = new Conexion();
 
+        try (Connection conexion = db_connect.get_connection()){
+            PreparedStatement ps=null;
+            try {
+                String query="INSERT INTO mensajes (mensaje, autor_mensaje, fecha_mensaje) VALUES (?,?,CURRENT_TIMESTAMP)"; //Parámetros con ? porque vienen del usuario.
+                ps=conexion.prepareStatement(query); //establecemos conexión.
+                ps.setString(1,mensaje.getMensaje()); //indicamos que enviamos parámetro 1 y lo traemos con get.
+                ps.setString(2,mensaje.getAutor_mensaje());
+                ps.executeUpdate(); //método que ejecuta la query.
+                System.out.println("Mensaje creado");
+            }catch (SQLException ex){
+                System.out.println(ex);
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
     }
 
     public static void leerMensajesDB() {
