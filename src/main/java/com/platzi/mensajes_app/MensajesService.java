21708b8facd348ce741f3e341b12a27b1aa817c4
Tuta <solmaruboldi@gmail.com>
2024-01-12 17:39:13 -0300

    Creación total del CRUD

--- src/main/java/com/platzi/mensajes_app/MensajesService.java
+++ src/main/java/com/platzi/mensajes_app/MensajesService.java
@@ -17,13 +17,25 @@ public class MensajesService {
         MensajesDAO.crearMensajeDB(registro);
     }
     public static void listarMensajes(){
-
+        MensajesDAO.leerMensajesDB();
     }
     public static void borrarMensaje(){
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Indica el ID del mensaje a borrar:");
+        int id_mensaje=sc.nextInt();
+        MensajesDAO.borrarMensajeDB(id_mensaje);
     }
     public static void editarMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu nuevo mensaje:");
+        String mensaje = sc.nextLine();
 
+        System.out.println("Indica el ID del mensaje a editar:");
+        int id_mensaje=sc.nextInt();
+        Mensajes actualizacion = new Mensajes();
+        actualizacion.setId_mensaje(id_mensaje);
+        actualizacion.setMensaje(mensaje);
+        MensajesDAO.actualizarMensajeDB(actualizacion);
     }
 
     /**
